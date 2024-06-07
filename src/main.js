8400e843498ba1d996cc3ad3b1f5567691854331
Denys Severin <severindenys367@gmail.com>
2024-06-07 13:00:55 +0200

    add: mapping englishbooks delete html files and recreate them in js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,15 @@
 import "./styles/main.scss";
 const apiKey = import.meta.env.VITE_API_KEY;
 
-const bookInfoImage = document.getElementById("bookInfo__image");
 const inputBtn = document.getElementById("form__button");
-const bookInfoTitle = document.querySelector(".bookInfo__title");
-const bookInfoAuthor = document.querySelector(".bookInfo__author");
-const bookInfoDescription = document.querySelector(
-  ".bookInfo__description"
-);
-const bookInfoPageCount = document.querySelector(
-  ".bookInfo__pageCount"
-);
 
 // display the input API request
 
 inputBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const inputElValue = document.querySelector(".form__input").value;
-
   const booksUrl = `https://www.googleapis.com/books/v1/volumes?q=${inputElValue}&key=${apiKey}`;
+
   const booksInformation = async () => {
     await fetch(booksUrl)
       .then((response) => {
@@ -30,33 +21,82 @@ inputBtn.addEventListener("click", (e) => {
       .then((userData) => {
         // Process the retrieved user data
         console.log(userData);
-        revealBackground();
-        bookInfoAuthor.innerText =
-          userData.items[0].volumeInfo.authors;
-        bookInfoTitle.innerText = userData.items[0].volumeInfo.title;
-        bookInfoDescription.innerText =
-          userData.items[0].volumeInfo.description;
-        bookInfoPageCount.innerText = `${userData.items[0].volumeInfo.pageCount} pages`;
 
-        // creating an image to google books
-        if (userData.items[0].volumeInfo.imageLinks) {
-          bookInfoImage.src =
-            userData.items[0].volumeInfo.imageLinks.thumbnail;
-        } else {
-          console.log("There is no image cover");
-        }
+        // SEARCH FOR THE BOOK FUNCTIONALITY
+
+        const bookContainer = document.getElementById("book-container");
+        const bookItems = userData.items;
+        const bookAuthor = bookItems[0].volumeInfo.authors;
+        const bookTitle = bookItems[0].volumeInfo.title;
+        const bookImg = bookItems[0].volumeInfo.imageLinks.thumbnail;
+        const bookDescription = bookItems[0].volumeInfo.description;
+        const bookPageCount = `${bookItems[0].volumeInfo.pageCount} pages`;
+
+        // create a bookInfoContainer
+        const bookInfoContainer = document.createElement("div");
+        bookInfoContainer.classList.add("bookInfo");
+
+        // create bookInfoTitle
+        const bookInfoTitle = document.createElement("h1");
+        bookInfoTitle.classList.add("bookInfo__title");
+        bookInfoTitle.innerText = bookTitle;
+        bookInfoContainer.appendChild(bookInfoTitle);
+
+        // create bookInfoAuthor
+        const bookInfoAuthor = document.createElement("h2");
+        bookInfoAuthor.classList.add("bookInfo__author");
+        bookInfoAuthor.innerText = bookAuthor;
+        bookInfoContainer.appendChild(bookInfoAuthor);
+
+        // create bookInfoImage
+        const bookInfoImg = document.createElement("img");
+        bookInfoImg.classList.add("bookInfo__imageContainer--image");
+        bookInfoImg.src = bookImg;
+        bookInfoContainer.appendChild(bookInfoImg);
+
+        // create bookInfoDescription
+        const bookInfoDescription = document.createElement("h2");
+        bookInfoDescription.classList.add("bookInfo__description");
+        bookInfoDescription.innerText = bookDescription;
+        bookInfoContainer.appendChild(bookInfoDescription);
+
+        // create bookInfoPages
+        const bookInfoPages = document.createElement("h3");
+        bookInfoPages.classList.add("bookInfo__pageCount");
+        bookInfoPages.innerText = bookPageCount;
+        bookInfoContainer.appendChild(bookInfoPages);
+
+        bookContainer.appendChild(bookInfoContainer);
 
         // filter english language books
-        const booksEnglish = userData.items;
-        console.log(booksEnglish);
 
-        const filteredEnglishBooks = booksEnglish.filter(
-          (bookEnglish) => {
-            return bookEnglish.volumeInfo.language === "en";
-            // why it needs to be return?
-          }
-        );
+        const filteredEnglishBooks = bookItems.filter((englishBook) => {
+          return englishBook.volumeInfo.language === "en";
+        });
+
         console.log(filteredEnglishBooks);
+
+        // mapping individual books in english
+        const mappedBooksInEnglish = () => {
+          filteredEnglishBooks.map((englishBook) => {
+            const booksContainer = document.createElement("div");
+            booksContainer.classList.add("bookInfo");
+            //create image Element
+            let imgElement = document.createElement("img");
+            imgElement.classList.add("bookInfo__imageContainer--image");
+            imgElement.src = englishBook.volumeInfo.imageLinks.thumbnail;
+            booksContainer.appendChild(imgElement);
+
+            // create description
+            const bookDescription = document.createElement("h2");
+            bookDescription.innerText = englishBook.volumeInfo.description;
+            bookDescription.classList.add("bookInfo__description");
+            booksContainer.appendChild(bookDescription);
+
+            bookContainer.appendChild(booksContainer);
+          });
+        };
+        mappedBooksInEnglish();
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -71,9 +111,4 @@ inputBtn.addEventListener("click", (e) => {
 
 const resetInput = () => {
   document.querySelector(".form__input").value = "";
-};
-
-const revealBackground = () => {
-  const bookInfoContainer = document.querySelector(".bookInfo");
-  bookInfoContainer.style.visibility = "visible";
 };
