1175b606968ba6a58478f2b4fb79c07eb9a61b8d
Denys Severin <severindenys367@gmail.com>
2024-12-30 10:17:33 +0100

    json configurations additions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from "vite";
+// export default defineConfig({
+//   // base: "/book-project/",
+//   base: "/",
+//   build: {
+//     outDir: "dist",
+//   },
+// });
+import { resolve } from "path";
 import { defineConfig } from "vite";
+
 export default defineConfig({
-  // base: "/book-project/",
-  base: "/",
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "details.html"),
+      },
+    },
   },
 });
